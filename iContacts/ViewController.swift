4f7c433d05e73996fad62d650e11cc7138c2b54a
Zhen <william.zhen.yan@gmail.com>
2020-06-22 21:26:28 +0900

    Initial Commit

---
+++ iContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iContacts
+//
+//  Created by Zhen YAN on 2020/06/22.
+//  Copyright © 2020 Zhen YAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
